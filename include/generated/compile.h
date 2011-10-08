8e721c91070b08cede17de6e4ce1cfa2471ed5a5
Takazumi Matsumoto <takazumi@gmail.com>
2011-10-08 05:04:50 +0000

    Initial commit
    Stock kernel.

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sat Oct 8 04:26:16 UTC 2011"
+#define LINUX_COMPILE_TIME "04:26:16"
+#define LINUX_COMPILE_BY "taks"
+#define LINUX_COMPILE_HOST "electrike"
+#define LINUX_COMPILE_DOMAIN "mshome.net"
+#define LINUX_COMPILER "gcc version 4.6.2 20110908 (prerelease) (Linaro GCC 4.6-2011.09-1) "
