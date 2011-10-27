5657e394a87eca0dd186bfb0f055b213f06b56d4
Takazumi Matsumoto <takazumi@gmail.com>
2011-10-27 11:37:58 +0000

    Cleanup

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Thu Oct 13 07:05:49 UTC 2011"
-#define LINUX_COMPILE_TIME "07:05:49"
-#define LINUX_COMPILE_BY "taks"
-#define LINUX_COMPILE_HOST "electrike"
-#define LINUX_COMPILE_DOMAIN "mshome.net"
-#define LINUX_COMPILER "gcc version 4.6.2 20110908 (prerelease) (Linaro GCC 4.6-2011.09-1) "
